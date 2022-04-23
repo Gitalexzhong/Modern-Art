ba5c88de3e7ac778c569963a643c64c249c100d5
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-04-23 23:06:38 +1000

    Added solve method parameter

--- painter.py
+++ painter.py
@@ -1,13 +1,15 @@
 
 
 
-def paint(fname):
+def paint(fname, solve_method = None):
     f = open(f"Maps/{fname}", "r")
     l, b, quota = f.readline().split(' ')
 
     arr = [[char for char in line] for line in f.read().splitlines()]
 
     print(arr)
+
+    print(solve_method)
 
 
 if __name__ == "__main__":
