c797ce89824365d493ff5f0b191f851510cb5324
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-04-24 18:49:57 +1000

    Added combinatorics solver

--- painter.py
+++ painter.py
@@ -1,6 +1,10 @@
 
 
 
+import itertools
+import math
+
+
 def paint(fname, solve_method = None):
     f = open(f"Maps/{fname}", "r")
     l, b, quota = f.readline().split(' ')
@@ -10,6 +14,25 @@ def paint(fname, solve_method = None):
     print(arr)
 
     print(solve_method)
+
+    cords = [(aq, ab) for aq in range(int(l)) for ab in range(int(b))]
+
+    comb = list(itertools.combinations(cords, int(quota)))
+
+    m = 0
+
+    for test in comb:
+        s = 0 
+        for i, j in test:
+            s += int(arr[i][j])
+
+        sa = round(min(1, math.exp(0.0015*(s-7500)))*100,10)
+        
+        if sa > m:
+            m = sa
+            ma = s
+    
+    print(m, ma)
 
 
 if __name__ == "__main__":
