1d5e213ce94fe0d6f65fe09ddae82fdb556f0501
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-04-29 23:17:38 +1000

    Added compuation to imported modules

--- painter.py
+++ painter.py
@@ -14,25 +14,15 @@ def paint(fname, solve_method = None):
 
     arr = [[char for char in line] for line in f.read().splitlines()]
 
-    # alg.brutev1.solve(arr, quota)
-    # algorithm.brutev1.solve(arr, quota)
-    # solve(arr, quota)
-    # brutev1()
-
     print("hiii")
 
-    importlib.import_module("algorithm." + solve_method)
+    a = importlib.import_module("algorithm." + solve_method)
 
-    # import algorithm.brutev1 
-    # import algorithm.{solve_method}
-
-    
+    a.solve(arr, quota)
     
 
 if __name__ == "__main__":
     start = time.time()
     paint("sample10_1", "brutev1")
     print(f"Seconds: {time.time() - start}")
-    # algorithm.brutev1.solve("arr", "quota")
-    # algo.brutev1.solve("arr", "quota")
 
