30f557ff8550842812037da81796a6d4dfa8427b
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-04 23:59:55 +1000

    Changed str to int returner

--- utility.py
+++ utility.py
@@ -1,14 +1,6 @@
 # Utility function for the painter function  
 from colorama import Fore, Style
 
-# Function to check if each selection of cord are connected to another selection cord
-def wrapped_tester(cords):
-    for x, y in cords:
-        if len(set([(x-1, y), (x+1, y), (x, y-1), (x, y+1)]) & set(cords)) == 0:
-            return False
-    
-    return True
-
 # Inputs a filename and return a array of values, the length, breadth and size of map to make
 def import_map_2d(filename):
     f = open(f"Maps/{filename}", "r")
@@ -16,7 +8,15 @@ def import_map_2d(filename):
 
     arr = [[char for char in line] for line in f.read().splitlines()]
 
-    return l, b, quota, arr
+    return int(l), int(b), quota, arr
+
+# Function to check if each selection of cord are connected to another selection cord
+def wrapped_tester(cords):
+    for x, y in cords:
+        if len(set([(x-1, y), (x+1, y), (x, y-1), (x, y+1)]) & set(cords)) == 0:
+            return False
+    
+    return True
 
 # Takes in a masked cords and outputs to stdout the masked area
 def grid_mask_print(arr, l, b, mask, export_type):
