a6dc4a92e95590604ddd0c4da0a4bb154d8d5b0a
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-05 00:00:15 +1000

    Changed quote to int

--- painter.py
+++ painter.py
@@ -1,15 +1,15 @@
 import importlib
 import time
-from utility import grid_coloured_print, grid_mask_print, import_map_2d
+from utility import grid_mask_print, import_map_2d
 
 def paint(fname, solve_method = None, export_type = None):
     l, b, quota, arr = import_map_2d(fname)
 
-    algo = importlib.import_module("algorithm." + solve_method)
+    # algo = importlib.import_module("algorithm." + solve_method)
 
-    mask = algo.solve(arr, quota)
+    # mask = algo.solve(arr, quota)
 
-    grid_mask_print(arr, int(l), int(b), mask, export_type)
+    # grid_mask_print(arr, int(l), int(b), mask, export_type)
 
 if __name__ == "__main__":
     
