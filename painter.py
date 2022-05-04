e07e37bf8946f61ec9c1892ce4aded23899b60c4
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-05 00:10:23 +1000

    Modified system fore core usage

--- painter.py
+++ painter.py
@@ -5,16 +5,14 @@ from utility import grid_mask_print, import_map_2d
 def paint(fname, solve_method = None, export_type = None):
     l, b, quota, arr = import_map_2d(fname)
 
-    # algo = importlib.import_module("algorithm." + solve_method)
+    algo = importlib.import_module("algorithm." + solve_method)
+    mask, score = algo.solve(l, b, arr, quota)
 
-    # mask = algo.solve(arr, quota)
-
-    # grid_mask_print(arr, int(l), int(b), mask, export_type)
+    grid_mask_print(arr, l, b, mask, score, export_type)
 
 if __name__ == "__main__":
     
     start = time.time()
     paint("sample10_1", "brutev1", "grid_coloured_print")
-    # paint("sample100_1", "brutev1")
     print(f"Seconds: {time.time() - start}")
 
