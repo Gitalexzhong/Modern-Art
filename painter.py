78a24e45a2121e50ea0714251ade5843be07438a
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-15 18:29:49 +1000

    Modified naming scheme for accuracy based xy

--- painter.py
+++ painter.py
@@ -8,12 +8,12 @@ algo_name = sys.argv[2]
 output_name = sys.argv[3]
 
 def paint():
-    l, b, quota, arr = import_map_2d(map_name)
+    size_v, size_h, quota, arr = import_map_2d(map_name)
 
     algo = importlib.import_module("algorithm." + algo_name)
-    mask, score = algo.solve(l, b, arr, quota)
+    mask, score = algo.solve(size_v, size_h, arr, quota)
 
-    grid_mask_print(arr, l, b, mask, score, output_name)
+    grid_mask_print(arr, size_v, size_h, mask, score, output_name)
 
 if __name__ == "__main__":
     start_time = time.time()
