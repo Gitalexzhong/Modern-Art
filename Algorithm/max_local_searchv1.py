a0935e9772956d88503a89f3f61f29a68c572704
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-15 20:42:30 +1000

    Added PQ function for usage

--- Algorithm/max_local_searchv1.py
+++ Algorithm/max_local_searchv1.py
@@ -4,38 +4,48 @@ import math
 import random
 from utility import random_combination, wrapped_tester
 
-def solve(l, b, arr, quota):
+def solve(height, length, arr, quota):
     
-    max_value = max([max(x) for x in arr])
+    max_value = max([max(val) for val in arr])
     listCordsMax = [];
+
     returnListCords = []
-    visitedMap = []
+
+    visitedMap = {}
     pq = {}
 
     # get max values list 
-    for i in range(l):
-        for j in range(b):
+    for i in range(height):
+        for j in range(length):
             if arr[i][j] == max_value:
                 listCordsMax.append((i, j))
 
     chosenStart = random.choice(listCordsMax)
     returnListCords.append(chosenStart)
-    visitedMap.append(chosenStart)
+    visitedMap[chosenStart] = True
 
-    testCord = chosenStart
+    # testCord = 
     # print(random.choice(listCordsMax))
     # print(returnListCords)
 
     print(visitedMap)
-    print(testCord[0], testCord[1])
-    x, y = testCord
+    # print(testCord[0], testCord[1])
+    x, y = chosenStart
     for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
     #     # if direction in cords:
     #         # queue.append(direction)
     #         # cords.remove(direction)
-        if a in range(0, l) and b in range(0, b):
+        # print(a,b)
+        # if a in range(0, l) and b in range(0, b) and ((6, 2)) not in visitedMap:       
+        if a in range(0, height) and b in range(0, length) and (a, b) not in visitedMap:
 
-            print(a, b)
+            # print(a,b, arr[a][b])
+            if arr[a][b] in pq:
+                pq[arr[a][b]].append((a, b))
+            else: 
+                pq[arr[a][b]] = [(a,b)]
+        
+    print(pq)
 
 
     # pq[(1,1)] = 5
