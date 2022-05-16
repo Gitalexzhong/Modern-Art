06ef10b28eb4f16685b8e442aec76dd4750e98c6
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-16 22:41:51 +1000

    Created New Local code algo

--- Algorithm/max_local_searchv1.py
+++ Algorithm/max_local_searchv1.py
@@ -2,6 +2,7 @@
 
 import math
 import random
+from re import M
 from utility import random_combination, wrapped_tester
 
 def solve(height, length, arr, quota):
@@ -13,6 +14,7 @@ def solve(height, length, arr, quota):
 
     visitedMap = {}
     pq = {}
+    score = 0
 
     # get max values list 
     for i in range(height):
@@ -28,7 +30,7 @@ def solve(height, length, arr, quota):
     # print(random.choice(listCordsMax))
     # print(returnListCords)
 
-    print(visitedMap)
+    # print(visitedMap)
     # print(testCord[0], testCord[1])
     x, y = chosenStart
     for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
@@ -45,11 +47,30 @@ def solve(height, length, arr, quota):
             else: 
                 pq[arr[a][b]] = [(a,b)]
         
-    print(pq)
-    print(max(pq.keys()))
-
-
-    # pq[(1,1)] = 5
     # print(pq)
+    # print()
 
-    return returnListCords, -1
+    for _ in range(quota - 1):
+        maxKey = max(pq.keys())
+
+        # print(maxKey)
+        # print(pq)
+
+        newSelected = random.choice(pq[maxKey])
+        visitedMap[newSelected] = True
+        pq[maxKey].remove(newSelected)
+        if pq[maxKey] == []:
+            del pq[maxKey]
+
+        score += int(maxKey)
+
+        x, y = newSelected
+        for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:      
+            if a in range(0, height) and b in range(0, length) and (a, b) not in visitedMap:
+
+                if arr[a][b] in pq:
+                    pq[arr[a][b]].append((a, b))
+                else: 
+                    pq[arr[a][b]] = [(a,b)]
+
+    return visitedMap, score
