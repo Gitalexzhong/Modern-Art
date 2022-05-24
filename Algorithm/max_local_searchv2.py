f9704d6e8872306262e04a1a5e4f86e12d20f44e
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-24 21:28:46 +1000

    Bug with overlap issue fixed

--- Algorithm/max_local_searchv2.py
+++ Algorithm/max_local_searchv2.py
@@ -4,7 +4,7 @@
 import math
 import random
 from re import M
-from utility import random_combination, wrapped_tester
+from utility import random_combination, wrapped_tester, grid_mask_print
 
 def solve(height, length, arr, quota):
     
@@ -16,6 +16,8 @@ def solve(height, length, arr, quota):
     for i in range(height):
         for j in range(length):
             if arr[i][j] == max_value:
+                return solve_local_based(height, length, arr, quota, (i, j))
+
                 visitedMap, score = solve_local_based(height, length, arr, quota, (i, j))
                 if score > bestScore:
                     bestMap, bestScore = visitedMap, score
@@ -24,36 +26,50 @@ def solve(height, length, arr, quota):
 
 def solve_local_based(height, length, arr, quota, chosenStart):
     visitedMap, pq = {}, {}
-    visitedMap[chosenStart] = True
+    visitedMap[chosenStart] = 1
     score = 0
-
+    # print(chosenStart)
+    # print(visitedMap)
     pq = check_visited(height, length, chosenStart, pq, visitedMap, arr)
 
+    print(pq)
+    print(visitedMap)
+    grid_mask_print(arr, length, height, visitedMap, -1, "grid_coloured_print", quota)
+    print()
     for i in range(quota - 1):
+        
         maxKey = max(pq.keys())
         newSelected = random.choice(pq[maxKey])
         
-        visitedMap[newSelected] = True
+        visitedMap[newSelected] = i + 2
         pq[maxKey].remove(newSelected)
         if pq[maxKey] == []:
             del pq[maxKey]
 
         score += int(maxKey)
+        # print(maxKey)
 
         pq = check_visited(height, length, newSelected, pq, visitedMap, arr)
+        print(pq)
+        print(visitedMap)
+        grid_mask_print(arr, length, height, visitedMap, -1, "grid_coloured_print", quota)
+        print()
 
-        print(i)
+    #     print(i)
+    # print(visitedMap)
     return visitedMap, score
 
 
 def check_visited(height, length, chosen, pq, visitedMap, arr):
     x, y = chosen
-    for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:      
-        if a in range(0, height) and b in range(0, length) and (a, b) not in visitedMap:
-
+    print(chosen)
+    for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:     
+        print("asd: " + str(a) + " " + str(b)) 
+        if a in range(0, height) and b in range(0, length) and (a, b) not in visitedMap and (arr[a][b] not in pq or (a, b) not in pq[arr[a][b]]):
+            print("input")
             if arr[a][b] in pq:
                 pq[arr[a][b]].append((a, b))
             else: 
-                pq[arr[a][b]] = [(a,b)]
+                pq[arr[a][b]] = [(a, b)]
     
     return pq
