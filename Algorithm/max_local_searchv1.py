21dce0290490fcf58fa246aa70d54bf2306b3892
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-24 21:57:37 +1000

    Updated to fix error

--- Algorithm/max_local_searchv1.py
+++ Algorithm/max_local_searchv1.py
@@ -42,12 +42,11 @@ def solve(height, length, arr, quota):
 
 def check_visited(height, length, chosen, pq, visitedMap, arr):
     x, y = chosen
-    for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:      
-        if a in range(0, height) and b in range(0, length) and (a, b) not in visitedMap:
-
+    for a, b in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:     
+        if a in range(0, height) and b in range(0, length) and (a, b) not in visitedMap and (arr[a][b] not in pq or (a, b) not in pq[arr[a][b]]):
             if arr[a][b] in pq:
                 pq[arr[a][b]].append((a, b))
             else: 
-                pq[arr[a][b]] = [(a,b)]
+                pq[arr[a][b]] = [(a, b)]
     
     return pq
