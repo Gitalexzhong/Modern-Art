6c4a071f23192a1b71bc96a7e21698d97a3c1dde
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-25 22:06:00 +1000

    Changed Utiility to remove Output functions

--- utility.py
+++ utility.py
@@ -33,36 +33,6 @@ def wrapped_tester(cords):
     else: 
         return True
 
-# Takes in a masked cords and outputs to stdout the masked area
-def grid_mask_print(arr, l, b, mask, score, export_type, quota):
-    if export_type == None or export_type == "BNW": 
-
-        for i in range(l):
-            for j in range(b):
-
-                if (i, j) in mask:
-                    print('#', end='')
-                else: 
-                    print('.', end='')
-
-            print('')
-
-    elif export_type == "grid_coloured_print": 
-        
-        for i in range(l):
-            for j in range(b):
-
-                if (i, j) in mask:
-                    print(Fore.RED + str(arr[i][j]) + Style.RESET_ALL, end='')
-                else: 
-                    print(arr[i][j], end='')
-
-            print('')
-
-    print(f"Score: {score}")
-    print(f"Weighted Score: {get_weighed_max_score(score, arr, quota)}%")
-    return
-
 # Custom code to generate random combinatorics of cords
 def random_combination(iterable, r):
     pool = tuple(iterable)
