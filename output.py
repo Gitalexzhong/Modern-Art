d702618a6d933ca54301350aa05b556ab31e28c4
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-26 21:25:36 +1000

    Function output for reusability

--- output.py
+++ output.py
@@ -2,34 +2,42 @@
 
 from utility import get_weighed_max_score
 from colorama import Fore, Style
-import math
 
 # Takes in a masked cords and outputs to stdout the masked area
-def grid_mask_print(arr, l, b, mask, score, export_type, quota):
-    if export_type == None or export_type == "BNW": 
+def output_result(arr, length, height, mask, score, export_type, quota):
+    print_algo = export_type.lower()
 
-        for i in range(l):
-            for j in range(b):
+    if print_algo == "grid_coloured_print": 
+        grid_coloured_print(length, height, arr, mask)
+    else: 
+        basic_print(length, height, mask)
 
-                if (i, j) in mask:
-                    print('#', end='')
-                else: 
-                    print('.', end='')
+    print_scores(score, arr, quota)
 
-            print('')
+    return
 
-    elif export_type == "grid_coloured_print": 
-        
-        for i in range(l):
-            for j in range(b):
-
-                if (i, j) in mask:
-                    print(Fore.RED + str(arr[i][j]) + Style.RESET_ALL, end='')
-                else: 
-                    print(arr[i][j], end='')
-
-            print('')
-
+def print_scores(score, arr, quota):
     print(f"Score: {score}")
     print(f"Weighted Score: {get_weighed_max_score(score, arr, quota)}%")
-    return
+
+def basic_print(length, height, mask):
+    for i in range(length):
+        for j in range(height):
+
+            if (i, j) in mask:
+                print('#', end='')
+            else: 
+                print('.', end='')
+
+        print('')
+
+def grid_coloured_print(length, height, arr, mask):
+    for i in range(length):
+        for j in range(height):
+
+            if (i, j) in mask:
+                print(Fore.RED + str(arr[i][j]) + Style.RESET_ALL, end='')
+            else: 
+                print(arr[i][j], end='')
+
+        print('')
