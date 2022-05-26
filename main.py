3e2f8fb57747e1ef4131330b1e612981e2188134
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-26 21:25:26 +1000

    Improved var names

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import importlib
 import sys
 import time
-from output import grid_mask_print
+from output import output_result
 from utility import import_map_2d
 
 map_name = sys.argv[1]
@@ -9,12 +9,12 @@ algo_name = sys.argv[2]
 output_name = sys.argv[3]
 
 def main():
-    size_v, size_h, quota, arr = import_map_2d(map_name)
+    length , height, quota, arr = import_map_2d(map_name)
     
     algo = importlib.import_module("algorithm." + algo_name)
-    mask, score = algo.solve(size_v, size_h, arr, quota)
+    mask, score = algo.solve(length, height, arr, quota)
 
-    grid_mask_print(arr, size_v, size_h, mask, score, output_name, quota)
+    output_result(arr, length, height, mask, score, output_name, quota)
 
 if __name__ == "__main__":
     start_time = time.time()
