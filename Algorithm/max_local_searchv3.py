28bd35ae597f25fa1f295f4e3eb66838caf90262
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-05-28 22:00:35 +1000

    Changes to validation system

--- Algorithm/max_local_searchv3.py
+++ Algorithm/max_local_searchv3.py
@@ -1,23 +1,31 @@
 # Local search based on select a max value first
 # Compared to V1, searches will be based on a nearby utility value
 
+from math import sqrt
+import operator
 import random
+
+incremental_step_value = 1
 
 def solve(height, length, arr, quota):
     print("Algo: Max Local Search 3.0")
     max_value = max([max(val) for val in arr])
-    bestScore = 0
-    bestMap = None
+    dict_max = {}
+    # dict_max = {(1, 1): 0, (2, 2): 0}
+    # bestScore = 0
+    # bestMap = None
 
     # get max values list 
-    for i in range(height):
-        for j in range(length):
-            if arr[i][j] == max_value:
-                visitedMap, score = solve_local_based(height, length, arr, quota, (i, j))
-                if score > bestScore:
-                    bestMap, bestScore = visitedMap, score
+    # for i in range(height):
+    #     for j in range(length):
+    #         if arr[i][j] == max_value:
+    #             dict_max[(i,j)] = 0
 
-    return bestMap, bestScore
+    # print(dict_max)
+    chosen_node = best_node_chooser(arr, dict_max)
+    # return [(1,1)], -1
+    return solve_local_based(height, length, arr, quota, chosen_node)
+
 
 def solve_local_based(height, length, arr, quota, chosenStart):
     visitedMap, pq = {}, {}
@@ -52,3 +60,41 @@ def check_visited(height, length, chosen, pq, visitedMap, arr):
                 pq[arr[a][b]] = [(a, b)]
     
     return pq
+
+def best_node_chooser(arr, node_list): 
+    max_range = incremental_step_value
+
+    while (1):
+        for x, y in node_list:
+            for item_x in range(len(arr)): 
+                for item_y in range(len(arr[0])): 
+                    distance = sqrt(pow((x - item_x), 2) + pow((y - item_y), 2))
+                    if distance <= max_range: 
+                        node_list[(x, y)] += int(arr[item_x][item_y])
+
+
+        node_list[(3,3)] = 26
+        # a = max(node_list.items(), key=operator.itemgetter(1))
+        # print(a)
+        # print()
+
+        max_item_val = max(node_list.values())
+        list_max = [key for key in node_list if node_list[key] == max_item_val]
+        # print(max_item_val)
+        # print(list_max)
+        # print(node_list)
+
+        if len(list_max) == 1:
+            print(list_max[0], max_item_val)
+            return list_max[0]
+        # print("pass")
+        max_range += incremental_step_value
+
+        # TODO add a bypass system for overload 
+
+    return None
+
+if __name__ == '__main__': 
+    arr = [['0', '3', '5', '2', '2'], ['0', '9', '4', '6', '8'], ['5', '2', '9', '7', '8'], ['0', '4', '4', '1', '6'], ['2', '3', '8', '0', '7']]
+    node_list = {(1, 1): 0, (2, 2): 0}
+    best_node_chooser(arr, node_list)
