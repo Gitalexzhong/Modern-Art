548ee3b3f592db9196ca5711577b1d58ffafa16b
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-01 21:49:31 +1000

    Added a heat output function

--- output.py
+++ output.py
@@ -51,19 +51,22 @@ def grid_heat_print(length, height, arr, mask):
     minV, maxV = get_min_max(arr)
     diff = maxV - minV
 
-    # for i in range(length):
-    #     for j in range(height):
+    for i in range(length):
+        for j in range(height):
 
-    #         if (i, j) in mask:
-    #             print(Fore.RED + str(arr[i][j]) + Style.RESET_ALL, end='')
-    #         else: 
-    #             print(arr[i][j], end='')
+            r,g,b = rgb(minV, maxV, int(arr[i][j]))
+            if (i, j) in mask:    
+                print("\x1b[38;2;"+str(r)+";"+str(g)+";"+str(b)+"m" + arr[i][j] + '\x1b[0m', end='')
+            else: 
+                print(arr[i][j], end='')
 
-    #     print('')
-    colors = [x for x in dir(Fore) if x[0] != "_"]
-    colors = [i for i in colors if i not in ["BLACK", "RESET"] and "LIGHT" not in i] 
+        print('')
 
-    for color  in colors:
-        print(colored(color, color.lower()))
+def rgb(minimum, maximum, value):
+    minimum, maximum = float(minimum), float(maximum)
+    ratio = 2 * (value-minimum) / (maximum - minimum)
+    b = int(max(0, 255*(1 - ratio)))
+    r = int(max(0, 255*(ratio - 1)))
+    g = 255 - b - r
 
-
+    return r, g, b
