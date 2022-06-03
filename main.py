cb6a40c3969aa5cfad846929920514672ebff371
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-03 23:14:57 +1000

    Modified main to use results recorder

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import importlib
 import sys
 import time
 from output import output_result
+from results import record_result
 from utility import import_map_2d
 
 map_name = sys.argv[1]
@@ -9,15 +10,20 @@ algo_name = sys.argv[2]
 output_name = sys.argv[3]
 
 def main():
-    length , height, quota, arr = import_map_2d(map_name)
+    start_time = time.time()
+
+    length, height, quota, arr = import_map_2d(map_name)
     
     algo = importlib.import_module("algorithm." + algo_name)
     mask, score = algo.solve(length, height, arr, quota)
 
-    output_result(arr, length, height, mask, score, output_name, quota)
+    weighted_score = output_result(arr, length, height, mask, score, output_name, quota)
+
+    total_time = time.time() - start_time
+    print("Seconds: " + str(total_time))
+
+    record_result(map_name, algo_name, output_name, total_time, score, weighted_score, length*height)
 
 if __name__ == "__main__":
-    start_time = time.time()
     main()
-    print("Seconds: " + str(time.time() - start_time))
     
