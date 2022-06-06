4aaa6e9dc0215986c8a17c5a89bb193bb4f87337
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-06 21:27:39 +1000

    Added more colors

--- animate.py
+++ animate.py
@@ -1,14 +1,20 @@
 # shows the process of the solver in an animated way 
 OKBLUE = '\033[94m'
 
-def animate_print(length, height, arr, red_mask = [], orange_mask = [], green_mask = [], blue_mask = []):
+def animate_print(length, height, arr, red_mask = [], yellow_mask = [], green_mask = [], blue_mask = []):
     printing = ""
 
     for i in range(length):
         for j in range(height):
 
             if (i, j) in red_mask:    
-                printing += "\033[94m" + arr[i][j] + '\x1b[0m'
+                printing += "\033[31m" + arr[i][j] + '\x1b[0m'
+            elif (i, j) in yellow_mask:    
+                printing += "\033[33m" + arr[i][j] + '\x1b[0m'
+            elif (i, j) in green_mask:    
+                printing += "\033[32m" + arr[i][j] + '\x1b[0m'
+            elif (i, j) in blue_mask:    
+                printing += "\033[334" + arr[i][j] + '\x1b[0m'
             else: 
                 printing += arr[i][j]
 
